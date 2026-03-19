12aa971845d6778c5da8bf8bdc2a0372251b75a5
juliansa99 <saaljulian@gmail.com>
2026-03-19 20:53:30 -0300

    Mejora la función saludo para incluir un mensaje personalizado

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function saludo(nombre, mensaje, hora) {
-console.log(`${mensaje}, ${nombre}. Son las ${hora}.`);
+// Función saludo usando template strings
+function saludo(nombre, mensaje) {
+console.log(`${mensaje}, ${nombre}!`);
 }
-saludo("Rosario", "Buenos días", "10:00 AM")
+// Ejemplos de uso:
+saludo("Rosario", "¡Hola");
+// Resultado: ¡Hola, Rosario!
+saludo("Pepe", "¡Buen trabajo"); // Resultado: ¡Buen trabajo, Pepe
